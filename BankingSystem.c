cd77fc3cacd357fc4504afa0004d649b5d1b29e1
Zaraar21-cloud <zararmohammed21@gmail.com>
2025-09-16 13:06:51 +0530

    Added account adding feature and initial deposit

--- BankingSystem.c
+++ BankingSystem.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
 @Zaraar
 Last Updated: 16-09-2025
 */
 
 typedef char string[20];
+int currentAccount = 0;
+int userAccount = 0;
 
 typedef struct {
     string name;
@@ -14,10 +17,41 @@ typedef struct {
 
 void startBankingSystem(){
     printf("Welcome to the Banking System\n");
+    int choice;
+    do {
+        
+        printf("Controls:\n 1) Create Account\n 2) Deposit\n 3) Withdraw\n 4) Check Balance\n 0) Exit\n");
+        printf("Choose: ");
+        scanf("%d", &choice);
+        if (choice > 0 && choice < 4) {
+           switch(choice){
+            case 1:
+            {
+                account newAccount;
+                printf("Enter your name: ");
+                scanf("%s", newAccount.name);
+                printf("Enter initial deposit: ");
+                scanf("%f", &newAccount.balance);
+                newAccount.accountNumber = currentAccount + 1;
+                currentAccount++;
+                printf("Account created successfully! Your account number is %d\n", newAccount.accountNumber);
+            }
+           }
+        }else{
+            if (choice == 0) {
+                printf("Exiting the Banking System...\n");
+                break;
+            } else {
+                printf("Please choose a valid option!\n");
+            }
+        }
+    }while(choice != 0);
+    
     
 }
 
 
 int main(){
-
+    startBankingSystem();
+    return 0;
 }
