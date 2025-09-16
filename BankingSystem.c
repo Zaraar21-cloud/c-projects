626c4783a6e1a619e1b8227577859b0c0b767abe
Zaraar21-cloud <zararmohammed21@gmail.com>
2025-09-16 12:59:05 +0530

    Started working on Banking System

---
+++ BankingSystem.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+@Zaraar
+Last Updated: 16-09-2025
+*/
+
+typedef char string[20];
+
+typedef struct {
+    string name;
+    int accountNumber;
+    float balance;
+} account;
+
+void startBankingSystem(){
+    printf("Welcome to the Banking System\n");
+    
+}
+
+
+int main(){
+
+}
